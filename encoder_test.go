9ab5b745c4bb82fe67d4dec00524514405a3fa1e
joeybloggs <Dean.Karn@gmail.com>
2016-06-19 23:13:02 -0400

    add uint & string tests

--- encoder_test.go
+++ encoder_test.go
@@ -189,3 +189,296 @@ func TestEncoderInt(t *testing.T) {
 	val, ok = values["NoPtrValue"]
 	Equal(t, ok, false)
 }
+
+func TestEncoderUint(t *testing.T) {
+
+	type TestUint struct {
+		Uint              uint
+		Uint8             uint8
+		Uint16            uint16
+		Uint32            uint32
+		Uint64            uint64
+		UintPtr           *uint
+		Uint8Ptr          *uint8
+		Uint16Ptr         *uint16
+		Uint32Ptr         *uint32
+		Uint64Ptr         *uint64
+		UintArray         []uint
+		UintPtrArray      []*uint
+		UintArrayArray    [][]uint
+		UintPtrArrayArray [][]*uint
+		UintMap           map[uint]uint
+		UintPtrMap        map[*uint]*uint
+		NoValue           uint
+		NoPtrValue        *uint
+	}
+
+	i := uint(3)
+	i8 := uint8(3)
+	i16 := uint16(3)
+	i32 := uint32(3)
+	i64 := uint64(3)
+
+	zero := uint(0)
+	one := uint(1)
+	two := uint(2)
+	three := uint(3)
+
+	test := TestUint{
+		Uint:              i,
+		Uint8:             i8,
+		Uint16:            i16,
+		Uint32:            i32,
+		Uint64:            i64,
+		UintPtr:           &i,
+		Uint8Ptr:          &i8,
+		Uint16Ptr:         &i16,
+		Uint32Ptr:         &i32,
+		Uint64Ptr:         &i64,
+		UintArray:         []uint{one, two, three},
+		UintPtrArray:      []*uint{&one, &two, &three},
+		UintArrayArray:    [][]uint{{one, zero, three}},
+		UintPtrArrayArray: [][]*uint{{&one, &zero, &three}},
+		UintMap:           map[uint]uint{one: three, zero: two},
+		UintPtrMap:        map[*uint]*uint{&one: &three, &zero: &two},
+	}
+
+	encoder := NewEncoder()
+	values, errs := encoder.Encode(test)
+
+	Equal(t, errs, nil)
+	Equal(t, len(values), 25)
+
+	val, ok := values["Uint8"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "3")
+
+	val, ok = values["Uint8"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "3")
+
+	val, ok = values["Uint16"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "3")
+
+	val, ok = values["Uint32"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "3")
+
+	val, ok = values["Uint64"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "3")
+
+	val, ok = values["Uint8"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "3")
+
+	val, ok = values["Uint8Ptr"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "3")
+
+	val, ok = values["Uint16Ptr"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "3")
+
+	val, ok = values["Uint32Ptr"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "3")
+
+	val, ok = values["Uint64Ptr"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "3")
+
+	val, ok = values["UintArray"]
+	Equal(t, ok, true)
+	Equal(t, len(val), 3)
+	Equal(t, val[0], "1")
+	Equal(t, val[1], "2")
+	Equal(t, val[2], "3")
+
+	val, ok = values["UintPtrArray[0]"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "1")
+
+	val, ok = values["UintPtrArray[1]"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "2")
+
+	val, ok = values["UintPtrArray[2]"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "3")
+
+	val, ok = values["UintArrayArray[0][0]"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "1")
+
+	val, ok = values["UintArrayArray[0][1]"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "0")
+
+	val, ok = values["UintArrayArray[0][2]"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "3")
+
+	val, ok = values["UintPtrArrayArray[0][0]"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "1")
+
+	val, ok = values["UintPtrArrayArray[0][1]"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "0")
+
+	val, ok = values["UintPtrArrayArray[0][2]"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "3")
+
+	val, ok = values["UintMap[0]"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "2")
+
+	val, ok = values["UintMap[1]"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "3")
+
+	val, ok = values["UintPtrMap[0]"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "2")
+
+	val, ok = values["UintPtrMap[1]"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "3")
+
+	val, ok = values["NoValue"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "0")
+
+	val, ok = values["NoPtrValue"]
+	Equal(t, ok, false)
+}
+
+func TestEncoderString(t *testing.T) {
+
+	type TestString struct {
+		String              string
+		StringPtr           *string
+		StringArray         []string
+		StringPtrArray      []*string
+		StringArrayArray    [][]string
+		StringPtrArrayArray [][]*string
+		StringMap           map[string]string
+		StringPtrMap        map[*string]*string
+		NoValue             string
+	}
+
+	one := "1"
+	two := "2"
+	three := "3"
+
+	test := TestString{
+		String:              three,
+		StringPtr:           &two,
+		StringArray:         []string{one, "", three},
+		StringPtrArray:      []*string{&one, nil, &three},
+		StringArrayArray:    [][]string{{one, "", three}, nil, {one}},
+		StringPtrArrayArray: [][]*string{{&one, nil, &three}, nil, {&one}},
+		StringMap:           map[string]string{one: three, three: two},
+		StringPtrMap:        map[*string]*string{&one: &three, &three: &two},
+	}
+
+	encoder := NewEncoder()
+	values, errs := encoder.Encode(test)
+
+	Equal(t, errs, nil)
+	Equal(t, len(values), 17)
+
+	val, ok := values["String"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "3")
+
+	val, ok = values["StringPtr"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "2")
+
+	val, ok = values["StringArray"]
+	Equal(t, ok, true)
+	Equal(t, len(val), 3)
+	Equal(t, val[0], "1")
+	Equal(t, val[1], "")
+	Equal(t, val[2], "3")
+
+	val, ok = values["StringPtr"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "2")
+
+	val, ok = values["StringPtrArray[0]"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "1")
+
+	val, ok = values["StringPtrArray[1]"]
+	Equal(t, ok, false)
+
+	val, ok = values["StringPtrArray[2]"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "3")
+
+	val, ok = values["StringPtrArray[2]"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "3")
+
+	val, ok = values["StringArrayArray[0][0]"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "1")
+
+	val, ok = values["StringArrayArray[0][1]"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "")
+
+	val, ok = values["StringArrayArray[0][2]"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "3")
+
+	val, ok = values["StringArrayArray[1][1]"]
+	Equal(t, ok, false)
+
+	val, ok = values["StringArrayArray[2][0]"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "1")
+
+	val, ok = values["StringPtrArrayArray[0][0]"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "1")
+
+	val, ok = values["StringPtrArrayArray[0][1]"]
+	Equal(t, ok, false)
+
+	val, ok = values["StringPtrArrayArray[0][2]"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "3")
+
+	val, ok = values["StringPtrArrayArray[1][1]"]
+	Equal(t, ok, false)
+
+	val, ok = values["StringPtrArrayArray[2][0]"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "1")
+
+	val, ok = values["StringMap[1]"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "3")
+
+	val, ok = values["StringMap[3]"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "2")
+
+	val, ok = values["StringPtrMap[1]"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "3")
+
+	val, ok = values["StringPtrMap[3]"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "2")
+
+	val, ok = values["NoValue"]
+	Equal(t, ok, true)
+	Equal(t, val[0], "")
+}
